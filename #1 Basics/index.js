d89575640ccc222ec280bedf9ee133380a9a1511
NeckI1T <nikitavasukov65@gmail.com>
2025-08-03 03:27:06 +0200

    #5 ЛКМ не ПКМ

--- #1 Basics/index.js
+++ #1 Basics/index.js
@@ -1,9 +1,9 @@
 // Reveal content on right-click
 // Prevent default context menu on first click and fade in the text pls
 
-document.addEventListener('contextmenu', function handler(event) {
+document.addEventListener('click', function handler(event) { // Im an idiot sorry
   event.preventDefault();
   const content = document.querySelector('.content');
   content.classList.add('visible');
-  document.removeEventListener('contextmenu', handler);
+  document.removeEventListener('click', handler); // ЛКМ блять и ПКМ перепутал 20 лет кстати
 });
